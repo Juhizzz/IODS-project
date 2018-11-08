3ce664a0fea99771d261771446259e36415621ab
Juhizzz <juho.ivaska@helsinki.fi>
2018-11-08 14:30:16 +0200

    Ex2_wrangling

---
+++ create_learning2014.R
@@ -0,0 +1,3 @@
+Juho Ivaska, 8.11.2018. This is the code for second excercise set of the course.
+read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", header = TRUE, sep = "\t")
+Looks like results of some kind of questionary.
