ba9a02504c134ac13a07e6baa109488fc3db24f1
Juhizzz <juho.ivaska@helsinki.fi>
2018-12-07 19:43:55 +0200

    Excersice6

---
+++ meet_and_repeat.R
@@ -0,0 +1,2 @@
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", header = TRUE, sep = " ")
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = T, sep = "\t")
